6d891d224c475c547a6c1df3ff706f735ae940ba
ChoiYoungHoon <dudgns2865@naver.com>
2022-09-19 21:48:48 +0900

    feat : Account, AccountUser domain 생성

---
+++ src/main/java/com/example/account/domain/Account.java
@@ -0,0 +1,51 @@
+package com.example.account.domain;
+
+import com.example.account.type.AccountStatus;
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Account {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    private AccountUser accountUser;
+
+    private String AccountNumber;
+
+    @Enumerated(EnumType.STRING)
+    private AccountStatus accountStatus;
+
+    private String accountNumber;
+
+    private LocalDateTime registeredAt;
+    private LocalDateTime updatedAt;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime unregisteredAt;
+}
---
+++ src/main/java/com/example/account/type/AccountStatus.java
@@ -0,0 +1,5 @@
+package com.example.account.type;
+
+public enum AccountStatus {
+    IN_USE, UNREGISTERED
+}
